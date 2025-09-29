530b8bf42b1460a7ce855e715801dca036ea647a
Eswin Paul <eswinpaul1994@gmail.com>
2025-09-29 19:55:17 +0530

    Update home.py

--- views/home.py
+++ views/home.py
@@ -70,8 +70,7 @@ if uploaded_file is not None:
         d = pd.ExcelFile(uploaded_file) 
         Data = pd.read_excel(d,sheet_name = "Form responses 1")
         data = Data[['Name (Fill the name in Capital Letters )', 'Second part of name/Initial(s)/ Second Name' ,'Date of Birth', 'Mobile Number','Email address','Sex', 'Blood Group','Emergency Contact  Mobile no','Address','Age (As on 25.02.2026)','Event 1', 'Event 2', 'Event 3','Member Confirmation']].copy()
-        data['Member Status'] = Data['Member Confirmation']
-        data['Member Status'] = data['Member Status'].str.lower().apply(lambda x: 'New' if 'New member' in x else 'Old')
+        data['Member Status'] = Data['Member Confirmation'].str.lower().apply(lambda x: 'New' if 'new member' in x else 'Old')
         # data['Member Status'] = (
 #     data['Member Status']
 #     .fillna('')  # Convert NaN to an empty string
@@ -127,6 +126,7 @@ if uploaded_file is not None:
           
 else:
     st.info("Please upload a CSV or XLSX file.")
+
 
 
 
